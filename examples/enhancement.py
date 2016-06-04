cd986a26356f16a7a55495e9948dd9873733a83e
Ranveer Aggarwal <ranveeraggarwal@gmail.com>
2016-06-04 15:59:20 +0530

    Project built

---
+++ examples/enhancement.py
@@ -0,0 +1,13 @@
+import cv2
+
+from enhancement.toonify import toonify
+
+img = cv2.imread('filename.jpg')
+
+if img is not None:
+    cv2.namedWindow('result', cv2.WINDOW_NORMAL)
+    cv2.imshow('result', toonify(img))
+    cv2.waitKey(0) & 0xFF
+    cv2.destroyAllWindows()
+else:
+    print("Image Doesn't Exist")
